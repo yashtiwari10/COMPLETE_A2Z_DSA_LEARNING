3a8cbc70d473b6b3caddedda4e56eb3ad1ed615f
Yash Tiwari <yt4369496@gmail.com>
2026-06-30 23:33:49 +0530

    Time: 0 ms (100%), Space: 94.9 MB (94.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        /* TC ----> O(N)
         vector<int> nums3;
         int i = 0; int j = 0;
         int n1 = nums1.size();
@@ -14,6 +15,43 @@ public:
 
         int n = n1 + n2;
         if(n % 2 == 1) return nums3[n/2];
-    return double((double)nums3[n/2] + (double)nums3[(n/2) - 1])/2.0;
+    return double((double)nums3[n/2] + (double)nums3[(n/2) - 1])/2.0; */
+
+        int n1 = nums1.size(); int n2 = nums2.size();
+        int n = n1 + n2;
+        int ind1 = n/2; int ind2 = ind1 - 1;
+        int cnt = 0;
+        int i = 0; int j = 0;
+        int el1 = -1; int el2 = -1;
+        while(i < n1 && j < n2){
+            if(nums1[i] < nums2[j]){
+                if(cnt == ind1) el1 = nums1[i];
+                if(cnt == ind2) el2 = nums1[i];
+                cnt++;
+                i++;
+            }
+            else{
+                if(cnt == ind1) el1 = nums2[j];
+                if(cnt == ind2) el2 = nums2[j];
+                cnt++;
+                j++;
+            }
+        }
+
+        while(i < n1){
+            if(cnt == ind1) el1 = nums1[i];
+            if(cnt == ind2) el2 = nums1[i];
+            cnt++;
+            i++;
+        }
+
+        while(j < n2){
+            if(cnt == ind1) el1 = nums2[j];
+            if(cnt == ind2) el2 = nums2[j];
+            cnt++;
+            j++;
+        }
+    if(n % 2 == 1) return el1;
+    return double(((double)el1 + (double)el2)/2); 
     }
 };
